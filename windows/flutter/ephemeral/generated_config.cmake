5e79f21a7bb5b6faf0dbe985caeb4285d2f84353
GrandezXaferdian <95590221+GrandezXaferdian@users.noreply.github.com>
2023-04-28 17:09:44 +0530

    Upload base app

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\rithv\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\rithv\\OneDrive\\Desktop\\separdianz" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\rithv\\flutter"
+  "PROJECT_DIR=C:\\Users\\rithv\\OneDrive\\Desktop\\separdianz"
+  "FLUTTER_ROOT=C:\\Users\\rithv\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rithv\\OneDrive\\Desktop\\separdianz\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\rithv\\OneDrive\\Desktop\\separdianz"
+  "FLUTTER_TARGET=C:\\Users\\rithv\\OneDrive\\Desktop\\separdianz\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\rithv\\OneDrive\\Desktop\\separdianz\\.dart_tool\\package_config.json"
+)
